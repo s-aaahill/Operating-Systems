3765abc87fd1a511efa938dac252a9baf69bddc9
Janvee31 <148671986+Janvee31@users.noreply.github.com>
2025-03-20 23:14:30 +0530

    Update class Deadlock toolkit App.py

--- class Deadlock toolkit App.py
+++ class Deadlock toolkit App.py
@@ -1,1 +1,8 @@
+class BankersAlgorithm:
+    def __init__(self, processes, resources, available, max_need, allocation):
+        self.processes = processes
+        self.resources = resources
+        self.available = available
+        self.max_need = max_need
+        self.allocation = allocation
 
