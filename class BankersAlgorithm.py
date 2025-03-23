5fd34cb637b54bda70844daeb28af2fa38af86ae
Kangan Sharma <149307108+kangans0@users.noreply.github.com>
2025-03-23 21:25:19 +0530

    Update class BankersAlgorithm.py

--- class BankersAlgorithm.py
+++ class BankersAlgorithm.py
@@ -6,5 +6,16 @@ class BankersAlgorithm:
         self.max_need = max_need
         self.allocation = allocation
         self.need = [[self.max_need[i][j] - self.allocation[i][j] for j in range(len(resources))] for i in range(len(processes))]
+    def is_safe(self):
+        work = self.available.copy()
+        finish = [False] * len(self.processes)
+        safe_sequence = []
+
+        while True:
+            found = False
+            for i in range(len(self.processes)):
+                if not finish[i] and all(self.need[i][j] <= work[j] for j in range(len(self.resources))):
+                    
+
 
 
