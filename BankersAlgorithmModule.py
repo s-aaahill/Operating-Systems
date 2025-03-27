11d7d19bd9db305e7c3ee83d485e2bebf5c09cf5
s-aaahill <148542431+s-aaahill@users.noreply.github.com>
2025-03-27 23:48:44 +0530

    Update and rename class BankersAlgorithm.py to BankersAlgorithmModule.py
    Final changes to the file

--- BankersAlgorithmModule.py
+++ BankersAlgorithmModule.py
@@ -5,7 +5,15 @@ class BankersAlgorithm:
         self.available = available
         self.max_need = max_need
         self.allocation = allocation
-        self.need = [[self.max_need[i][j] - self.allocation[i][j] for j in range(len(resources))] for i in range(len(processes))]
+        self.need = self._calculate_need()
+
+    def _calculate_need(self):
+        return [
+            [self.max_need[i][j] - self.allocation[i][j] 
+            for j in range(len(self.resources)) 
+            for i in range(len(self.processes))
+        ]
+
     def is_safe(self):
         work = self.available.copy()
         finish = [False] * len(self.processes)
@@ -19,14 +27,12 @@ class BankersAlgorithm:
                     finish[i] = True
                     safe_sequence.append(self.processes[i])
                     found = True
-             if not found:
+
+            if not found:
                 break
 
-        if all(finish):
-            return True, safe_sequence
-        else:
-            return False, []
-# Deadlock Detection
+        return (True, safe_sequence) if all(finish) else (False, [])
+
 def detect_deadlock(allocation, request, available):
     num_processes = len(allocation)
     num_resources = len(available)
@@ -40,39 +46,8 @@ def detect_deadlock(allocation, request, available):
                 work = [work[j] + allocation[i][j] for j in range(num_resources)]
                 finish[i] = True
                 found = True
+
         if not found:
             break
 
-    if all(finish):
-        return False  # No deadlock
-    else:
-        return True  # Deadlock detected
-# Resource Allocation Graph Visualization
-def draw_resource_allocation_graph(allocation, request, processes, resources):
-    G = nx.DiGraph()
-
-    # Add nodes for processes and resources
-    for process in processes:
-        G.add_node(process, type='process')
-    for resource in resources:
-        G.add_node(resource, type='resource')
-
-    # Add edges for allocation and request
-    for i in range(len(processes)):
-        for j in range(len(resources)):
-            if allocation[i][j] > 0:
-                G.add_edge(resources[j], processes[i])  # Resource -> Process
-            if request[i][j] > 0:
-                G.add_edge(processes[i], resources[j])  # Process -> Resource
-
-    # Draw the graph
-    pos = nx.spring_layout(G)
-    node_colors = ['lightblue' if G.nodes[node]['type'] == 'process' else 'lightgreen' for node in G.nodes]
-    nx.draw(G, pos, with_labels=True, node_color=node_colors, node_size=3000, font_size=10, font_weight='bold')
-    return G, pos
-
-
-                    
-
-
-
+    return not all(finish)
